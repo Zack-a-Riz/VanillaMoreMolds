9709f8325ac87abf975d711b5441c8c0327265d8
Zack-a-Riz <mail.zackariz@gmail.com>
2025-01-08 02:51:21 +0100

    Ajoutez des fichiers projet.

---
+++ VanillaMoreMolds/VanillaMoreMoldsModSystem.cs
@@ -0,0 +1,30 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace VanillaMoreMolds
+{
+    public class VanillaMoreMoldsModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("Hello from vanillamoremolds mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Notification(Lang.Get("vanillamoremolds:start"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.Logger.Notification(Lang.Get("vanillamoremolds:start"));
+        }
+
+    }
+}
+
